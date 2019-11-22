8cf8410ea3f7ddc5e673b503f36b1ada91b4be33
persona64 <seregagoev@gmail.com>
2019-11-22 17:33:46 +0300

    начал делать 1.11 но пока ничего не получается

---
+++ SergeyGoevTask1/Task1_11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_11
+{
+    class Program
+    {
+        private static int nextWord;
+
+        static void Main(string[] args)
+        {
+            string inputString = Console.ReadLine();
+            var arrayOfStrings = inputString.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+            int averageLenght = inputString.Aggregate(0, (count, nextWord) => count += nextWord) / inputString.Length;
+
+            Console.WriteLine("Средняя длина строки: {0}", averageLenght);
+
+            Console.ReadLine();
+        }
+    }
+}
