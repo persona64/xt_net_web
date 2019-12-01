3830a6729efc01ebeb8fcade3c5c229b32750af8
persona64 <seregagoev@gmail.com>
2019-12-01 14:46:26 +0300

    доделал задание 1.10

--- SergeyGoevTask1/Taks1_10/Program.cs
+++ SergeyGoevTask1/Taks1_10/Program.cs
@@ -11,15 +11,34 @@ namespace Taks1_10
         static void Main(string[] args)
         {
             Random rnd = new Random();
-            int[,] arrayOfNumbers = new int[4,4];
+            int[,] arrayOfNumbers = new int[4, 4];
+            int sum = 0;
 
-            for(int i = 0; i < arrayOfNumbers.Length; i++)
+            for(int i = 0; i < arrayOfNumbers.GetLength(0); i++)
             {
-                for(int j = 0; j < arrayOfNumbers.Length; j++)
+                Console.WriteLine();
+                for (int j = 0; j < arrayOfNumbers.GetLength(1); j++)
                 {
-
+                    arrayOfNumbers[i, j] = rnd.Next(10);
+                    Console.Write("{0,3}", arrayOfNumbers[i, j]);
                 }
             }
+            Console.WriteLine();
+
+            for(int i = 0; i < arrayOfNumbers.GetLength(0); i++)
+            {
+                Console.WriteLine();
+                for(int j = 0; j < arrayOfNumbers.GetLength(1); j++)
+                {
+                    if ((i + j) % 2 != 0) 
+                    {
+                        sum += arrayOfNumbers[i, j];
+                        Console.Write("{0,3}", arrayOfNumbers[i,j]);
+                    }
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
