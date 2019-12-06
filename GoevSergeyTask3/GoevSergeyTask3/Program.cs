d9324f0518fd678ebb55405cea3d4f75504da748
Гоев Сергей Александрович <seregagoev@gmail.com>
2019-12-06 16:16:09 +0400

    выполнил задания 3.1 3.2

---
+++ GoevSergeyTask3/GoevSergeyTask3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoevSergeyTask3
+{
+   
+
+    class Program
+    {
+        static void PrintList(IEnumerable<int> list)
+        {
+            foreach (var item in list)
+            {
+                Console.Write("{0} ", item);
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 };
+            bool delete = false;
+
+            while(list.Count > 1)
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (delete) list.RemoveAt(i--);
+                    delete = !delete;
+                }
+                PrintList(list);
+            }
+ 
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GoevSergeyTask3/Task3_2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3_2
+{
+    class Program
+    {
+        static void WordFrequency(string word)
+        {
+            char[] stringSeparator = { ' ', '.', '!', ',' };
+            string changeString = word.ToLower();
+            string[] wordsArray = changeString.Split(stringSeparator);
+            Dictionary<string, int> words = new Dictionary<string, int>();
+
+            foreach (var item in wordsArray)
+            {
+                if (words.ContainsKey(item))
+                    words[item]++;
+                else
+                    words.Add(item, 1);
+            }
+
+            Console.WriteLine();
+
+            foreach (KeyValuePair<string, int> keyValuePair in words)
+            {
+                Console.WriteLine("Слово: {0}, частота повторений: {1}", keyValuePair.Key, keyValuePair.Value);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string text = "In the summer it is hot. In the winter it is not. In the spring " +
+                "there are flowers. In the autumn there are showers.";
+            string text2 = "Rain on the green grass, Rain on the tree, " +
+                "Rain on the house - tops, But not on me";
+            string text3 = "Whether the weather be fine, Or whether the weather be not, " +
+                "Whether the weather be cold, Or whether the weather be hot, " +
+                "We’ll whether the weather Whatever the weather Whether we like it or not.";
+
+            WordFrequency(text);
+            WordFrequency(text2);
+            WordFrequency(text3);
+
+            Console.ReadKey();
+        }
+    }
+}
